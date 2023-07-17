d9304f2e347d3f701185fbb0d861f1de7d15ca41
lavanya <lavanyabogavarapu22@gmail.com>
2023-07-17 13:12:03 +0000

    Develop to approach Light Dark Mode changes Background color Conditional Rendering

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import LightDarkMode from './components/LightDarkMode'
+
+import './App.css'
+
+const App = () => <LightDarkMode />
+export default App
---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,38 @@
+// Write your code here
+
+import {Component} from 'react'
+
+import './index.css'
+
+class LightDarkMode extends Component {
+  state = {
+    isDarkMode: true,
+  }
+
+  onClickButtonText = () => {
+    this.setState(prevState => ({isDarkMode: !prevState.isDarkMode}))
+  }
+
+  render() {
+    const {isDarkMode} = this.state
+    const modeClassName = isDarkMode ? 'dark-mode' : 'light-mode'
+    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
+
+    return (
+      <div className="light-container">
+        <div className={`light-change-container ${modeClassName}`}>
+          <h1 className="heading">Click To Change Mode</h1>
+          <button
+            type="button"
+            onClick={this.onClickButtonText}
+            className="light-status-button"
+          >
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LightDarkMode
